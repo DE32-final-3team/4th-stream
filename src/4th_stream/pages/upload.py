63f35ca77f5bcfb7e0cd1a2688b4de8ad8391707
j25ng <jsan2861@gmail.com>
2024-10-25 15:11:32 +0900

    add model

--- src/4th_stream/pages/upload.py
+++ src/4th_stream/pages/upload.py
@@ -2,10 +2,12 @@ import streamlit as st
 from mtcnn import MTCNN
 from PIL import Image
 from requests.exceptions import ConnectionError
+from transformers import ViTFeatureExtractor, ViTForImageClassification
 import numpy as np
 import requests
 import os
 import io
+import torch
 
 age_mapping = {
     0: "0-2 years",
@@ -19,6 +21,20 @@ age_mapping = {
     8: "70+ years",
 }
 
+# Load the age classifier model
+feature_extractor = ViTFeatureExtractor.from_pretrained("nateraw/vit-age-classifier")
+model = ViTForImageClassification.from_pretrained("nateraw/vit-age-classifier")
+
+
+def predict_age(face_img):
+    # Preprocess face for the model
+    inputs = feature_extractor(images=face_img, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(**inputs)
+    predicted_class = outputs.logits.argmax(-1).item()
+    predicted_age = age_mapping[predicted_class]
+    return predicted_age
+
 
 def find_face(img):
     detector = MTCNN()
@@ -28,7 +44,7 @@ def find_face(img):
     return faces
 
 
-def upload_img(img, name):
+def upload_img(img, name, predicted_age):
     ip = os.getenv("EC2_IP", "localhost")
     url = f"http://{ip}:3003/uploadfile"
 
@@ -36,11 +52,9 @@ def upload_img(img, name):
     img.save(img_bytes, format="JPEG")
     img_bytes.seek(0)
 
-    p_result = 1
-
     params = {
         "file": (name + ".jpeg", img_bytes, "image/jpeg"),
-        "prediction_result": (None, p_result),
+        "prediction_result": (None, predicted_age),
     }
     r = requests.post(url, files=params)
     return r
@@ -64,10 +78,13 @@ if pic is not None:
             face_crop = image.crop((x - 15, y - 15, x + w + 15, y + h + 15))
             st.image(face_crop, width=100)
 
+            # Predict age
+            predicted_age = predict_age(face_crop)
+
             file_name = f"{pic.name.split('.')[-2]}({i+1})"
 
             try:
-                r = upload_img(face_crop, file_name)
+                r = upload_img(face_crop, file_name, predicted_age)
                 if r.status_code == 200:
                     st.success("이미지 업로드 성공!")
                 else:
