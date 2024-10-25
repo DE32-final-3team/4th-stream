38b35d0ad2f4378bcd74be9cbf7f98faa1000908
j25ng <jsan2861@gmail.com>
2024-10-25 09:30:26 +0900

    add dependency

---
+++ src/4th_stream/pages/all.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import os
+
+st.title("Result")
+
+age_mapping = {
+    0: "0-2",
+    1: "3-9",
+    2: "10-19",
+    3: "20-29",
+    4: "30-39",
+    5: "40-49",
+    6: "50-59",
+    7: "60-69",
+    8: "70+",
+}
+
+ip = os.getenv("EC2_IP", "localhost")
+
+
+def load_data():
+    url = f"http://{ip}:8022/all"
+    r = requests.get(url).json()
+    return r
+
+
+data = load_data()
+df = pd.DataFrame(data)
+
+for i in range(len(df)):
+    p = df["prediction_result"][i]
+    r = df["answer"][i]
+
+    if p is not None and r is not None:
+        with st.container(border=True):
+            img, info = st.columns([1, 3])
+            img.image(df["file_path"][i], width=100)
+            p_age = age_mapping[int(p)]
+            r_age = age_mapping[int(r)]
+            info.markdown("")
+            info.markdown("")
+            info.markdown(
+                f"사진 속 사람의 예상 나이대는 :blue-background[**{p_age}**] 입니다."
+            )
+            info.markdown(
+                f"사진 속 사람의 실제 나이대는 :red-background[**{r_age}**] 입니다."
+            )
---
+++ src/4th_stream/pages/upload.py
@@ -0,0 +1,73 @@
+import streamlit as st
+from mtcnn import MTCNN
+from PIL import Image
+from requests.exceptions import ConnectionError
+import numpy as np
+import requests
+import os
+import io
+
+age_mapping = {
+    0: "0-2 years",
+    1: "3-9 years",
+    2: "10-19 years",
+    3: "20-29 years",
+    4: "30-39 years",
+    5: "40-49 years",
+    6: "50-59 years",
+    7: "60-69 years",
+    8: "70+ years",
+}
+
+
+def find_face(img):
+    detector = MTCNN()
+
+    img_np = np.array(img)
+    faces = detector.detect_faces(img_np)
+    return faces
+
+
+def upload_img(img, name):
+    ip = os.getenv("EC2_IP", "localhost")
+    url = f"http://{ip}:8022/uploadfile/"
+
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format="JPEG")
+    img_bytes.seek(0)
+
+    files = {"file": (name + ".jpeg", img_bytes, "image/jpeg")}
+    r = requests.post(url, files=files)
+    return r
+
+
+st.title("Upload Picture")
+pic = st.file_uploader(
+    "img", type=["png", "jpg", "jpeg", "webp"], label_visibility="hidden"
+)
+
+if pic is not None:
+    image = Image.open(pic).convert("RGB")
+    faces = find_face(image)
+
+    if len(faces) == 0:
+        st.error("No faces detected.")
+    else:
+        for i, face in enumerate(faces):
+            x, y, w, h = face["box"]
+
+            face_crop = image.crop((x - 15, y - 15, x + w + 15, y + h + 15))
+            st.image(face_crop, width=100)
+
+            file_name = f"{pic.name.split('.')[-2]}({i+1})"
+
+            try:
+                r = upload_img(face_crop, file_name)
+                if r.status_code == 200:
+                    st.success("이미지 업로드 성공!")
+                else:
+                    st.warning("이미지 업로드에 실패하였습니다.")
+            except ConnectionError:
+                st.warning("연결 오류 발생 : 이미지 업로드에 실패하였습니다.")
+            except Exception as e:
+                st.error(f"알 수 없는 오류 발생: {e}")
